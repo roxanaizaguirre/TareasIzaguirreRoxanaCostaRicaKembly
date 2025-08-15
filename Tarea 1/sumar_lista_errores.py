15a2a13e44ef4a46bbb2171bb9dab63e01b8c746
roxanaizaguirre <roxanaizaguirre@estudiantec.cr>
2025-08-15 17:31:33 -0600

    Función suma de números, presenta errores de formato

---
+++ Tarea 1/sumar_lista_errores.py
@@ -0,0 +1,16 @@
+# Archivo: sumar_lista_correcion.py
+# Suma números de una lista
+
+
+numeros =[1, 2, 3, 4, 5]  # Números a sumar
+suma = sum(numeros)  # Función suma incorporada
+
+
+
+def mostrar_suma():
+    """Imprime el resultado de la suma."""
+    print("La suma es:", suma)
+
+
+# Llamada a la función 
+mostrar_suma()
