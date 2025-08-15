144702534da5ca63f2f7cf4870388425db1f2fa9
roxanaizaguirre <roxanaizaguirre@estudiantec.cr>
2025-08-15 17:32:04 -0600

    Función suma de números, errores corregidos

---
+++ Tarea 1/sumar_lista_corregido.py
@@ -0,0 +1,15 @@
+# Suma números de una lista
+
+# Correcion espacio entre el igual
+numeros = [1, 2, 3, 4, 5]
+suma = sum(numeros)
+
+
+# Correcion espacio entre lineas
+def mostrar_suma():
+    """Imprime el resultado de la suma."""
+    print("La suma es:", suma)
+
+
+# Llamada a la función y correcion de espacios finales
+mostrar_suma()
